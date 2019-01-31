6740d70b6a605db9edd7928e81b744ec1a4d9e3e
Oscar Gomez Soriano <ogomez@stratio.com>
2019-01-31 10:47:03 +0100

    google style fixed, code rearranged

--- src/main/java/com/stratio/sanitas/starter/StarterApplication.java
+++ src/main/java/com/stratio/sanitas/starter/StarterApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StarterApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StarterApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(StarterApplication.class, args);
+  }
 
 }
 
